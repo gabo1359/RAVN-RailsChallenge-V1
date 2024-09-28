109b5384f7f8846600ca9d689bcbc0a2ed2369b3
Gabriel <gabs1995@hotmail.com>
2024-09-28 14:00:52 -0500

    :sparkles: add pokemons controller and set root path

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,7 @@ Rails.application.routes.draw do
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "pokemons#index"
+
+  resources :pokemons, only: :index
 end
